fba6c5e1c4f9dca556695a52e10221cab1bed1e4
Aron Rissato <aron_rissato@hotmail.com>
2019-12-02 15:03:13 -0300

    Ajuste editar Conta

--- Bank/Controllers/ContaController.cs
+++ Bank/Controllers/ContaController.cs
@@ -82,13 +82,11 @@ namespace Bank.Controllers
         }
 
         // POST: Conta/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdConta,Nome,Descricao,Status,CriadoEm")] Conta conta)
+        public async Task<IActionResult> Edit(Conta conta)
         {
-            if (id != conta.IdConta)
+            if (conta.IdConta != conta.IdConta)
             {
                 return NotFound();
             }
